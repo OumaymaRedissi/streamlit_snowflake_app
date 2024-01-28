58b73224452454e272acb5472176dd077a98bc1e
OumaymaRedissi <49315041+OumaymaRedissi@users.noreply.github.com>
2024-01-28 18:20:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,8 @@ try:
 except URLError as e:
   streamlit.error()
 
+streamlit.header('View Our Fruit List - Add Your Favorites!')
+
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
